b3a7543156306ec60c0a3db8a43328845ee5f435
Dominic Fam <famdominic1218@gmail.com>
2024-09-17 14:53:43 +0800

    commit

--- pages/home.php
+++ pages/home.php
@@ -1,49 +1,42 @@
-<?php
+<?php 
 
-require 'parts/header.php';
+    // 1. connect to database
+        $database = connectToDB();
+    // 2. load the "publish" posts data
+        $sql = "SELECT 
+                    posts.id, posts.title, posts.content, posts.status, posts.user_id, users.name
+                    FROM posts 
+                    JOIN users 
+                    ON posts.user_id = users.id 
+                    WHERE posts.status = 'publish'";
+        $query = $database -> prepare($sql);
+        $query -> execute();
+        $posts = $query -> fetchAll();
 
-?>
-
+require "parts/header.php"; ?>
 <div class="container mx-auto my-5" style="max-width: 500px;">
-      <h1 class="h1 mb-4 text-center">My Blog</h1>
-      <div class="card mb-2">
+    <h1 class="h1 mb-4 text-center">My Blog</h1>
+    <!--- 
+    3. foreach all the posts 
+    - display the title
+    - display the content
+    - display the link (add a id into the link)
+    -->
+    <?php foreach ($posts as $post) : ?> 
+    <div class="card mb-2">
         <div class="card-body">
-          <h5 class="card-title">Post 4</h5>
-          <p class="card-text">Here's some content about post 4</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
+            <h5 class="card-title"><?= $post['title']; ?></h5>
+            <p class="card-text">
+                Author: <?= $post['name']; ?>
+            </p>
+            <div class="text-end">
+            <a href="/post?id=<?= $post['id']; ?>" class="btn btn-primary btn-sm">Read More</a>
+            </div>
         </div>
-      </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 3</h5>
-          <p class="card-text">This is for post 3</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 2</h5>
-          <p class="card-text">This is about post 2</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 1</h5>
-          <p class="card-text">This is a post</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
-    
-      <div class="mt-4 d-flex justify-content-center gap-3">
+    </div>
+    <?php endforeach ;?>
+
+    <div class="mt-4 d-flex justify-content-center gap-3">
     <?php if ( isset( $_SESSION['user'] ) ) : ?>
         <!-- show dashboard and logout link if user is logged in -->
         <a href="/dashboard" class="btn btn-link btn-sm">Dashboard</a>
@@ -54,8 +47,5 @@ require 'parts/header.php';
         <a href="/signup" class="btn btn-link btn-sm">Sign Up</a>
     <?php endif; ?>
     </div>
-   </div>
-    
-    <?php
-      require 'parts/footer.php';
-   ?>   
+</div>
+<?php require 'parts/footer.php';
--- pages/manage_posts.php
+++ pages/manage_posts.php
@@ -1,32 +1,35 @@
-<?php
+<?php 
 
-// check if whoever that viewing this page is logged in.
-// if not logged in, you want to redirect back to login page
-checkIfuserIsNotLoggedIn();
+  // check if whoever that viewing this page is logged in.
+  // if not logged in, you want to redirect back to login page
+  checkIfuserIsNotLoggedIn();
 
-// 1. connect to the database
-$database = connectToDB();
+  // 1. connect to the database
+  $database = connectToDB();
 
-// 2. get all the users
-// 2.1
-$sql = "SELECT * FROM posts";
-$sql2 = "SELECT * FROM `posts` WHERE `user_id` = '3'";
-// 2.2
-$query = $database->prepare( $sql );
-$query2 = $database->prepare( $sql2 );
-// 2.3
-$query->execute();
-$query2->execute();
-// 2.4
-$posts = $query->fetchAll();
-$posts2 = $query2->fetchAll();
+  // load data from the database
+  // if logged in user is not a admin or editor, show only their own posts
+  if ( $_SESSION['user']['role'] == 'user' ) {
+    $sql = "SELECT posts.id, posts.title, posts.content, posts.status, posts.user_id, users.name, posts.posted_on FROM posts JOIN users ON posts.user_id = users.id WHERE posts.user_id = :user_id";
+    $query = $database->prepare( $sql );
+    $query->execute([
+      "user_id" => $_SESSION['user']['id']
+    ]);
+    $posts = $query->fetchAll();
+  } else {
+    $sql =  "SELECT 
+                    posts.id, posts.title, posts.content, posts.user_id, users.name, posts.status ,posts.posted_on
+                    FROM posts 
+                    JOIN users 
+                    ON posts.user_id = users.id";
+    $query = $database->prepare( $sql );
+    $query->execute();
+    $posts = $query->fetchAll();
+    
+  }
 
-   require 'parts/header.php';
-?>
-
-<?php if ( $_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'editor' ) : ?>
-  
-    <div class="container mx-auto my-5" style="max-width: 700px;">
+require "parts/header.php"; ?>
+<div class="container mx-auto my-5" style="max-width: 700px;">
       <div class="d-flex justify-content-between align-items-center mb-2">
         <h1 class="h1">Manage Posts</h1>
         <div class="text-end">
@@ -40,112 +43,56 @@ $posts2 = $query2->fetchAll();
           <thead>
             <tr>
               <th scope="col">ID</th>
-              <th scope="col" style="width: 40%;">Title</th>
+              <th scope="col" style="width: 20%;">Title</th>
               <th scope="col">Status</th>
+              <th scope="col">Author</th>
+              <th scope="col">Posted On</th>
               <th scope="col" class="text-end">Actions</th>
             </tr>
           </thead>
           <tbody>
-            <!-- 3. use foreach to display all the posts -->
-            <?php foreach ($posts as $index => $posts) :?>
-             <tr>
-              <th scope="row"><?= $posts['id']?></th>
-              <td><?= $posts['title']?></td>
-              <td>
-                <?php if ( $posts['status'] == 'publish' ) : ?>
-                  <span class="badge bg-success">Publish</span>
-                <?php endif; ?>
-                <?php if ( $posts['status'] == 'pending' ) : ?>
-                  <span class="badge bg-warning">Pending Review</span>
-                <?php endif; ?>
-              </td>
-              <td class="text-end">
-                <div class="buttons">
-                  <a href="/post.php?id=<?= $posts['id']; ?>" class="btn btn-primary btn-sm me-2"><i class="bi bi-eye"></i></a>
-                  <a href="/manage-posts-edit?id=<?= $posts['id']; ?>" class="btn btn-success btn-sm me-2"><i class="bi bi-pencil"></i></a>
-
-                  <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delete-post-<?= $posts['id']; ?>">
-                    <i class="bi bi-trash"></i>
-                  </button>
-
-                  <!-- Modal -->
-                  <div class="modal fade" id="delete-post-<?= $posts['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabdel">Delete User: <?= $posts['title']; ?></h1>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body text-start">
-                          This action cannot be reversed.
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                          <form method="POST" action="/post/delete">
-                            <input type="hidden" name="id" value="<?= $posts['id']; ?>" />
-                            <button class="btn btn-danger"> <i class="bi bi-trash"></i> Delete Now</button>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </td>
-             </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-      </div>
-<?php else : ?>
-
-  <div class="container mx-auto my-5" style="max-width: 700px;">
-      <div class="d-flex justify-content-between align-items-center mb-2">
-        <h1 class="h1">Manage Posts</h1>
-        <div class="text-end">
-          <a href="/manage-posts-add" class="btn btn-primary btn-sm"
-            >Add New Post</a
-          >
-        </div>
-      </div>
-      <div class="card mb-2 p-4">
-        <table class="table">
-          <thead>
+          <?php foreach ($posts as $index => $post) :?>
             <tr>
-              <th scope="col">ID</th>
-              <th scope="col" style="width: 40%;">Title</th>
-              <th scope="col">Status</th>
-              <th scope="col" class="text-end">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <!-- 3. use foreach to display all the posts -->
-            <?php foreach ($posts2 as $index => $posts) :?>
-             <tr>
-              <th scope="row"><?= $index+1?></th>
-              <td><?= $posts['title']?></td>
+              <th scope="row"><?= $post['id']?></th>
+              <td><?= $post['title']?></td>
               <td>
-                <?php if ( $posts['status'] == 'publish' ) : ?>
-                  <span class="badge bg-success">Publish</span>
-                <?php endif; ?>
-                <?php if ( $posts['status'] == 'pending' ) : ?>
-                  <span class="badge bg-warning">Pending Review</span>
-                <?php endif; ?>
+                <?php if($post['status'] == "publish") :?>
+                  <span class="badge bg-success"><?=$post['status']?></span>
+                <?php else :?>
+                  <span class="badge bg-warning"><?=$post['status']?></span>
+                <?php endif ;?>
+              </td>
+              <td>
+                <!-- author -->
+                <?= $post['name']?>
+              </td>
+              <td>
+                <!-- Posted On date -->
+                <?= $post['posted_on']?>
               </td>
               <td class="text-end">
                 <div class="buttons">
-                  <a href="/post.php?id=<?= $posts['id']; ?>" class="btn btn-primary btn-sm me-2"><i class="bi bi-eye"></i></a>
-                  <a href="/manage-posts-edit?id=<?= $posts['id']; ?>" class="btn btn-success btn-sm me-2"><i class="bi bi-pencil"></i></a>
-
-                  <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delete-post-<?= $posts['id']; ?>">
+                  <a
+                    href="/post?id=<?= $post['id']; ?>"
+                    target="_blank"
+                    class="btn btn-primary btn-sm me-2"
+                    ><i class="bi bi-eye"></i
+                  ></a>
+                  <a
+                    href="/manage-posts-edit?id=<?= $post['id']; ?>"
+                    class="btn btn-secondary btn-sm me-2"
+                    ><i class="bi bi-pencil"></i
+                  ></a>
+                 <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delete-post-<?= $post['id']; ?>">
                     <i class="bi bi-trash"></i>
                   </button>
 
                   <!-- Modal -->
-                  <div class="modal fade" id="delete-post-<?= $posts['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal fade" id="delete-post-<?= $post['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabdel">Delete User: <?= $posts['title']; ?></h1>
+                          <h1 class="modal-title fs-5" id="exampleModalLabdel">Delete Post: <?= $post['title']; ?></h1>
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body text-start">
@@ -154,30 +101,26 @@ $posts2 = $query2->fetchAll();
                         <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                           <form method="POST" action="/post/delete">
-                            <input type="hidden" name="id" value="<?= $posts['id']; ?>" />
-                            <button class="btn btn-danger"> <i class="bi bi-trash"></i> Delete Now</button>
+                            <input type="hidden" name="id" value="<?= $post['id']; ?>" />
+                            <button class="btn btn-danger btn-sm"><i class="bi bi-trash"></i>Delete Post</button>
                           </form>
                         </div>
                       </div>
                     </div>
                   </div>
+                  
                 </div>
               </td>
-             </tr>
+            </tr>
             <?php endforeach; ?>
+            
           </tbody>
         </table>
       </div>
-
-<?php endif; ?>
-
       <div class="text-center">
         <a href="/dashboard" class="btn btn-link btn-sm"
           ><i class="bi bi-arrow-left"></i> Back to Dashboard</a
         >
       </div>
     </div>
-
-    <?php
-       require 'parts/footer.php';
-    ?>
+    <?php require 'parts/footer.php';
